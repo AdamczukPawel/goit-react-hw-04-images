3ed0548f1cb835ab24ca3f45494d5bd08690b647
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2023-02-04 20:51:32 +0100

    .

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
 export const fetchPicturesByTopic = async (searchQuery, page) => {
+  
   const response = await axios.get(
     '/?q=' +
       searchQuery +
